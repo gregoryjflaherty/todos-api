9740c2f5a5202bf860078de07aefa68236fc15d1
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-03-09 10:11:10 -0500

    Setup todo app - set up mopdel specs

---
+++ spec/models/item_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Item, type: :model do
+
+  describe 'relationships' do
+    it { should belong_to(:todo)}
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name)}
+  end
+end

f02d68151f3212567e28adfbe0a7458171ea8051
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-03-11 19:23:42 -0500

    Add config set up to rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,11 +30,11 @@ require 'rspec/rails'
 # [...]
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 # [...]
-RSpec.configuration do |config|
-  # [...]
-  config.include RequestSpecHelper, type: :request
-  # [...]
-end
+# RSpec.configuration do |config|
+#   # [...]
+#   config.include RequestSpecHelper, type: :request
+#   # [...]
+# end
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
@@ -46,7 +46,7 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 RSpec.configure do |config|
 
-
+  config.include RequestSpecHelper, type: :request
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
